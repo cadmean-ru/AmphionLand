a3d4889c1b4e27bc4ec723a36a969a652d2a8c7f
Georgy <kotlyar@cadmean.ru>
2021-02-06 13:49:30 +0300

    dont

--- AmphionLand/components/scrollManager.go
+++ AmphionLand/components/scrollManager.go
@@ -4,16 +4,35 @@ package components
 
 import (
 	"fmt"
+	"github.com/cadmean-ru/amphion/common"
 	"github.com/cadmean-ru/amphion/engine"
 	"syscall/js"
 )
 
 type Scrolling struct {
 	engine.ComponentImpl
+	realX, realY, sceneX, sceneY float32
+
 }
 
 func (s *Scrolling) OnInit(ctx engine.InitContext) {
 	s.ComponentImpl.OnInit(ctx)
+
+	sceneSize :=s.Engine.GetCurrentScene().Transform.GetSize()
+
+	s.sceneX, s.sceneY = sceneSize.X, sceneSize.Y
+	s.realX, s.realY = 0, 0
+
+
+	s.SceneObject.ForEachObject(func(object *engine.SceneObject) {
+		if object.Transform.GetGlobalRect().X.Max > s.realX {
+			s.realX = object.Transform.GetGlobalRect().X.Max
+		}
+		if object.Transform.GetGlobalRect().Y.Max > s.realY {
+			s.realX = object.Transform.GetGlobalRect().X.Max
+		}
+	})
+
 
 	js.Global().Get("document").Call("addEventListener", "wheel", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		var event = args[0]
@@ -25,8 +44,8 @@ func (s *Scrolling) OnInit(ctx engine.InitContext) {
 		engine.LogDebug(fmt.Sprintf("deltaY: %f", deltaY))
 		engine.LogDebug(fmt.Sprintf("deltaX: %f", deltaX))
 
-		s.SceneObject.Transform.Position.Y += float32(deltaY)
-		s.SceneObject.Transform.Position.X += float32(deltaX)
+		s.SceneObject.Transform.Position.X += common.ClampFloat32(float32(deltaX), 0,s.realX)
+		s.SceneObject.Transform.Position.Y += common.ClampFloat32(float32(deltaY), 0,s.realY)
 
 		s.SceneObject.ForEachObject(func(object *engine.SceneObject) {
 			object.ForEachComponent(func(component engine.Component) {
