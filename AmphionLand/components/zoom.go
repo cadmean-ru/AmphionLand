48c94a0001629bac956a1ca13bfa46021e5d7162
Stacy Smith <nastakuz2015@gmail.com>
2021-02-06 15:17:16 +0300

    Zoom
    Text goes zoom

---
+++ AmphionLand/components/zoom.go
@@ -0,0 +1,50 @@
+//+build js
+
+package components
+
+import (
+	"github.com/cadmean-ru/amphion/common/a"
+	"github.com/cadmean-ru/amphion/engine"
+	"github.com/cadmean-ru/amphion/engine/builtin"
+)
+
+type Zooming struct {
+	engine.ComponentImpl
+	ZoomNumber int `state`
+	EventOn int `state`
+	EventOff int `state`
+}
+
+func (s *Zooming) OnInit(ctx engine.InitContext) {
+	s.ComponentImpl.OnInit(ctx)
+	s.SceneObject.AddComponent(builtin.NewEventListener(s.EventOn,
+		func(nokeyDownEvent engine.AmphionEvent) bool{
+			for _, component:= range s.SceneObject.GetComponentsByName(".*TextView"){
+				if view, ok := component.(*builtin.TextView); ok {
+					view.FontSize *= byte(s.ZoomNumber)
+					view.ForceRedraw()
+					engine.RequestRendering()
+				}
+			}
+			s.SceneObject.SetSize(s.SceneObject.Transform.Size.Multiply(a.Vector3{X: float32(s.ZoomNumber),
+				Y: float32(s.ZoomNumber), Z: float32(s.ZoomNumber)}))
+			return true
+		}))
+	s.SceneObject.AddComponent(builtin.NewEventListener(s.EventOff,
+		func(keyDownEvent engine.AmphionEvent) bool{
+			for _, component:= range s.SceneObject.GetComponentsByName(".*TextView"){
+				if view, ok := component.(*builtin.TextView); ok {
+					view.FontSize /= byte(s.ZoomNumber)
+					view.ForceRedraw()
+					engine.RequestRendering()
+				}
+			}
+			s.SceneObject.SetSize(s.SceneObject.Transform.Size.Multiply(a.Vector3{X: 1/float32(s.ZoomNumber),
+				Y: 1/float32(s.ZoomNumber), Z: 1/float32(s.ZoomNumber)}))
+			return true
+		}))
+}
+
+func (s *Zooming) GetName() string {
+	return engine.NameOfComponent(s)
+}
--- AmphionLand/go.mod
+++ AmphionLand/go.mod
@@ -3,4 +3,3 @@ module AmphionLand
 go 1.15
 
 require github.com/cadmean-ru/amphion v0.1.6
-
