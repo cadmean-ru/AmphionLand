f1b68a8fa2f62bc7f932242c0f6ff9e13ef4a7b1
Aleksey Kritskov <krickov.aleksey@cadmean.ru>
2021-04-15 15:31:30 +0300

    Big fix

--- AmphionLand/components/editorController.go
+++ AmphionLand/components/editorController.go
@@ -15,14 +15,16 @@ func (s *EditorController) OnInit(ctx engine.InitContext) {
 	s.ComponentImpl.OnInit(ctx)
 
 	sceneObject1 := engine.NewSceneObject("bruh1")
-	sceneObject1.Transform.Size = a.NewVector3(a.MatchParent,a.MatchParent,1)
+	sceneObject1.Transform.Position.Z = 1
+	sceneObject1.Transform.Size = a.NewVector3(a.MatchParent,a.MatchParent,0)
 	view := builtin.NewShapeView(builtin.ShapeRectangle)
 	view.FillColor = a.NewColor(100, 100, 100)
 	view.StrokeWeight = 0
 	sceneObject1.AddComponent(view)
 
 	sceneObject2 := engine.NewSceneObject("bruh2")
-	sceneObject2.Transform.Size = a.NewVector3(a.MatchParent,a.MatchParent,1)
+	sceneObject2.Transform.Position.Z = 1
+	sceneObject2.Transform.Size = a.NewVector3(a.MatchParent,a.MatchParent,0)
 	layout := builtin.NewGridLayout()
 	layout.Cols = 2
 	layout.RowPadding = 20
@@ -32,34 +34,26 @@ func (s *EditorController) OnInit(ctx engine.InitContext) {
 
 	if err==nil{
 		prefab.Transform.Size = a.NewVector3(a.MatchParent,50,4)
-		view2 := builtin.NewShapeView(builtin.ShapeRectangle)
-		view2.FillColor = a.NewColor(102, 102, 153)
-		view2.CornerRadius = 69
 		textView := prefab.GetChildByName("Button text").GetComponentByName(".+TextView").(*builtin.TextView)
 		textView.SetText("Save Prefab")
 		textView.SetHTextAlign(a.TextAlignCenter)
 		textView.SetVTextAlign(a.TextAlignCenter)
-		prefab.AddComponent(view2)
 		sceneObject2.AddChild(prefab)
 	}
 
 	prefab2, err2 := engine.LoadPrefab(res.Prefabs_button)
 
 	if err2==nil{
-
 		prefab2.Transform.Size = a.NewVector3(a.MatchParent,50,4)
-		view3 := builtin.NewShapeView(builtin.ShapeRectangle)
-		view3.FillColor = a.NewColor(102, 102, 153)
-		view3.CornerRadius = 69
 		textView := prefab2.GetChildByName("Button text").GetComponentByName(".+TextView").(*builtin.TextView)
 		textView.SetText("Save Scene")
 		textView.SetHTextAlign(a.TextAlignCenter)
 		textView.SetVTextAlign(a.TextAlignCenter)
-		prefab2.AddComponent(view3)
 		sceneObject2.AddChild(prefab2)
 	}
 
 	sceneObject2_3 := engine.NewSceneObject("bruh3")
+	sceneObject2_3.Transform.Position.Z = 1
 	sceneObject2_3.Transform.Size = a.NewVector3(a.MatchParent,a.MatchParent,1)
 	view4 := builtin.NewShapeView(builtin.ShapeRectangle)
 	view4.FillColor = a.NewColor(115, 115, 180)
@@ -68,6 +62,7 @@ func (s *EditorController) OnInit(ctx engine.InitContext) {
 	sceneObject2.AddChild(sceneObject2_3)
 
 	sceneObject2_4 := engine.NewSceneObject("bruh4")
+	sceneObject2_3.Transform.Position.Z = 1
 	sceneObject2_4.Transform.Size = a.NewVector3(a.MatchParent,a.MatchParent,1)
 	view5 := builtin.NewShapeView(builtin.ShapeRectangle)
 	view5.FillColor = a.NewColor(115, 115, 180)
