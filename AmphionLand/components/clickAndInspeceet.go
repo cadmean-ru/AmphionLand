22ca795c0788a118ad745a0fb15a4e8bc9538e36
Aleksey Kritskov <krickov.aleksey@cadmean.ru>
2021-05-20 14:47:35 +0300

    bruh

--- AmphionLand/components/clickAndInspeceet.go
+++ AmphionLand/components/clickAndInspeceet.go
@@ -1,11 +1,10 @@
 package components
 
 import (
-	"AmphionLand/generated/res"
+	"fmt"
 	"github.com/cadmean-ru/amphion/common/a"
 	"github.com/cadmean-ru/amphion/engine"
 	"github.com/cadmean-ru/amphion/engine/builtin"
-	"strconv"
 )
 
 type ClickAndInspeceet struct {
@@ -83,6 +82,10 @@ func (s *ClickAndInspeceet) handleClick(event engine.AmphionEvent) bool {
 	s.showInspector(s.SceneObject)
 
 	engine.RequestRendering()
+
+	engine.LogDebug("Click handled")
+	engine.LogDebug("Engine state%s", s.Engine.GetStateString())
+
 	return false
 }
 
@@ -99,86 +102,86 @@ func (s *ClickAndInspeceet) showInspector(object *engine.SceneObject) {
 	objectNameBox.AddComponent(objectNameBoxText)
 	s.hierarchy.AddChild(objectNameBox)
 
-	//transMap := map[string]a.Vector3 {
-	//	"Position": object.Transform.Position,
-	//	"Rotation": object.Transform.Rotation,
-	//	"Size":     object.Transform.Size,
-	//}
-	//
-	//i := 0
-	//for vecName, vec := range transMap {
-	//	nameBox := engine.NewSceneObject("nameBox" + string(rune(i)))
-	//	nameBox.Transform.Size.Y = 30
-	//	nameBoxText := builtin.NewTextView(vecName)
-	//	nameBoxText.SetVTextAlign(a.TextAlignCenter)
-	//	nameBoxText.SetHTextAlign(a.TextAlignCenter)
-	//	nameBox.AddComponent(nameBoxText)
-	//	s.hierarchy.AddChild(nameBox)
-	//
-	//	box := engine.NewSceneObject("emptyBox" + string(rune(i)))
-	//	box.Transform.Size.Y = 90
-	//	grid := builtin.NewGridLayout()
-	//	grid.Cols = 2
-	//	grid.Rows = 3
-	//
-	//	box.AddComponent(grid)
-	//
-	//	nameMap := [3]string{"X", "Y", "Z"}
-	//	valueMap := [3]float32{vec.X, vec.Y, vec.Z}
-	//
-	//	for j := 0; j < 3; j++{
-	//		name := engine.NewSceneObject("name" + string(rune(i)) + string(rune(j)))
-	//		nameText := builtin.NewTextView(nameMap[j])
-	//		nameText.SetVTextAlign(a.TextAlignCenter)
-	//		nameText.SetHTextAlign(a.TextAlignCenter)
-	//
-	//		name.AddComponent(nameText)
-	//		name.Transform.Size.Y = 30
-	//
-	//		value := engine.NewSceneObject("value" + string(rune(i)) + string(rune(j)))
-	//		valueText := builtin.NewTextView(strconv.FormatFloat(float64(valueMap[j]), 'f',3,32))
-	//		valueText.SetVTextAlign(a.TextAlignCenter)
-	//		valueText.SetHTextAlign(a.TextAlignCenter)
-	//
-	//		value.AddComponent(valueText)
-	//		value.Transform.Size.Y = 30
-	//		//value.AddComponent(builtin.NewShapeView(builtin.ShapeRectangle))
-	//
-	//		box.AddChild(name)
-	//		box.AddChild(value)
-	//	}
-	//	s.hierarchy.AddChild(box)
-	//
-	//	i++
-	//}
+	transMap := map[string]a.Vector3 {
+		"Position": object.Transform.Position,
+		"Rotation": object.Transform.Rotation,
+		"Size":     object.Transform.Size,
+	}
 
-	if object.GetName() == "Horizontal grid" {
-		gridObject := engine.NewSceneObject("grid bruh")
-		colsAmount := object.GetComponentByName("GridLayout").(*builtin.GridLayout).Rows
+	i := 0
+	for vecName, vec := range transMap {
+		nameBox := engine.NewSceneObject(fmt.Sprintf("nameBox %d", i))
+		nameBox.Transform.Size.Y = 30
+		nameBoxText := builtin.NewTextView(vecName)
+		nameBoxText.SetVTextAlign(a.TextAlignCenter)
+		nameBoxText.SetHTextAlign(a.TextAlignCenter)
+		nameBox.AddComponent(nameBoxText)
+		s.hierarchy.AddChild(nameBox)
 
+		box := engine.NewSceneObject(fmt.Sprintf("emptyBox %d", i))
+		box.Transform.Size.Y = 90
 		grid := builtin.NewGridLayout()
 		grid.Cols = 2
-		grid.Rows = 1
+		grid.Rows = 3
 
-		gridObject.AddComponent(grid)
+		box.AddComponent(grid)
 
-		inputObj, _ := engine.LoadPrefab(res.Prefabs_inputBox)
-		buttonObj, _ := engine.LoadPrefab(res.Prefabs_button)
+		nameMap := [3]string{"X", "Y", "Z"}
+		valueMap := [3]float32{vec.X, vec.Y, vec.Z}
 
-		inputText := inputObj.FindComponentByName("TextView").(*builtin.TextView)
-		inputText.SetText(strconv.Itoa(colsAmount))
+		for j := 0; j < 3; j++{
+			name := engine.NewSceneObject(fmt.Sprintf("name %d %d", i, j))
+			nameText := builtin.NewTextView(nameMap[j])
+			nameText.SetVTextAlign(a.TextAlignCenter)
+			nameText.SetHTextAlign(a.TextAlignCenter)
 
-		buttonObj.FindComponentByName("TextView").(*builtin.TextView).SetText("ok")
+			name.AddComponent(nameText)
+			name.Transform.Size.Y = 30
 
-		buttonObj.AddComponent(builtin.NewEventListener(engine.EventMouseDown, func(event engine.AmphionEvent) bool {
-			inputObjText,_ := strconv.Atoi(inputText.GetText())
-			object.FindComponentByName("GridLayout").(*builtin.GridLayout).Cols = inputObjText
-			return true
-		}))
+			value := engine.NewSceneObject(fmt.Sprintf("value %d %d", i, j))
+			valueText := builtin.NewTextView(fmt.Sprintf("%f", valueMap[j]))
+			valueText.SetVTextAlign(a.TextAlignCenter)
+			valueText.SetHTextAlign(a.TextAlignCenter)
 
-		gridObject.AddChild(inputObj)
-		gridObject.AddChild(buttonObj)
+			value.AddComponent(valueText)
+			value.Transform.Size.Y = 30
+			//value.AddComponent(builtin.NewShapeView(builtin.ShapeRectangle))
 
-		s.hierarchy.AddChild(gridObject)
+			box.AddChild(name)
+			box.AddChild(value)
+		}
+		s.hierarchy.AddChild(box)
+
+		i++
 	}
+
+	//if object.GetName() == "Horizontal grid" {
+	//	gridObject := engine.NewSceneObject("grid bruh")
+	//	colsAmount := object.GetComponentByName("GridLayout").(*builtin.GridLayout).Rows
+	//
+	//	grid := builtin.NewGridLayout()
+	//	grid.Cols = 2
+	//	grid.Rows = 1
+	//
+	//	gridObject.AddComponent(grid)
+	//
+	//	inputObj, _ := engine.LoadPrefab(res.Prefabs_inputBox)
+	//	buttonObj, _ := engine.LoadPrefab(res.Prefabs_button)
+	//
+	//	inputText := inputObj.FindComponentByName("TextView").(*builtin.TextView)
+	//	inputText.SetText(strconv.Itoa(colsAmount))
+	//
+	//	buttonObj.FindComponentByName("TextView").(*builtin.TextView).SetText("ok")
+	//
+	//	buttonObj.AddComponent(builtin.NewEventListener(engine.EventMouseDown, func(event engine.AmphionEvent) bool {
+	//		inputObjText,_ := strconv.Atoi(inputText.GetText())
+	//		object.FindComponentByName("GridLayout").(*builtin.GridLayout).Cols = inputObjText
+	//		return true
+	//	}))
+	//
+	//	gridObject.AddChild(inputObj)
+	//	gridObject.AddChild(buttonObj)
+	//
+	//	s.hierarchy.AddChild(gridObject)
+	//}
 }
